3e771e3c0aa593dff8e9aa2a99b0f946e1924596
Arturo Escamilla García <ing.arturoescamilla@outlook.com>
2020-05-25 10:20:55 -0500

    add ballPainter

---
+++ Assets/InkPainter/Sample/Script/BallPaint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Es.InkPainter;
+
+public class BallPaint : MonoBehaviour
+{
+
+    [SerializeField]
+    private Brush brush = null;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("floor"))
+        {
+            var canvas = other.GetComponent<InkCanvas>();
+            if (canvas != null)
+                canvas.Paint(brush, transform.position);
+
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/InkPainter/Sample/Script/MoveBall.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBall : MonoBehaviour {
+
+	float currentTime = 0;
+	float currentTimeFall = 0;
+	public float timeLimit = 2;
+	public float timeLimitFall = 0.4f;
+	public GameObject objectFall;
+
+	int move = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		currentTime += Time.deltaTime;
+		currentTimeFall+= Time.deltaTime;
+
+		if (currentTime >= timeLimit)
+		{
+			move *= -1;
+			currentTime = 0;
+		}
+
+		transform.Translate(Vector3.left * Time.deltaTime * move);
+
+		if (currentTimeFall >= timeLimitFall)
+		{
+			currentTimeFall = 0;
+			Instantiate(objectFall, this.transform);
+		}
+
+
+		////////////////////////////////////////////////////////////
+
+		// if (currentTime >= timeChange && !m_FacingRight)
+		// 	{
+        //         // Reset Time
+        //         currentTime = 0;
+        //         move = -move;
+		// 		// ... flip the player.
+		// 		Flip();
+		// 	}
+		// 	// Otherwise if the input is moving the player left and the player is facing right...
+		// 	else if (currentTime >= timeChange && m_FacingRight)
+		// 	{
+        //         // Reset Time
+        //         currentTime = 0;
+        //         move = move * -1;
+
+		// 		// ... flip the player.
+		// 		Flip();
+		// 	}
+	}
+}
